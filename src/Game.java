7674b05d461a4c957ad728efc788ff1d865d2500
Chris <chris.mueller256@gmail.com>
2019-04-14 14:41:33 -0500

    Formatting

--- src/Game.java
+++ src/Game.java
@@ -8,41 +8,33 @@ import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
+
 /**
  * @author panos
  */
-public class Game extends BasicGame
-{
-    public Game()
-    {
-        super("Wizard game");
+public class Game extends BasicGame {
+    public Game() {
+        super("Game");
     }
 
-    public static void main(String[] arguments)
-    {
-        try
-        {
+    public static void main(String[] arguments) {
+        try {
             AppGameContainer app = new AppGameContainer(new Game());
-            app.setDisplayMode(500, 400, false);
+            app.setDisplayMode(1920, 1080, true);
             app.start();
-        }
-        catch (SlickException e)
-        {
+        } catch (SlickException e) {
             e.printStackTrace();
         }
     }
 
     @Override
-    public void init(GameContainer container) throws SlickException
-    {
+    public void init(GameContainer container) throws SlickException {
     }
 
     @Override
-    public void update(GameContainer container, int delta) throws SlickException
-    {
+    public void update(GameContainer container, int delta) throws SlickException {
     }
 
-    public void render(GameContainer container, Graphics g) throws SlickException
-    {
+    public void render(GameContainer container, Graphics g) throws SlickException {
     }
 }
